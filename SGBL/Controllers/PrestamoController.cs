aac654cfeae090a90cef51c89aab30b359679dd2
LuisLachapel <luislachapelcentropeguero@gmail.com>
2024-03-14 22:58:55 -0400

    Insert y select hechos

---
+++ SGBL/Controllers/PrestamoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Accesodatos.Context;
+using Accesodatos.Tablas;
+using MediatR;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Aplicación.Logica.Prestamo;
+
+namespace SGBL.Controllers
+{
+    public class PrestamoController : GeneralController
+    {
+        [HttpPost]
+        public async Task<ActionResult<Unit>> Insertar(Insertar.EjecutaPrestamo datos)
+        {
+            return await Mediator.Send(datos);
+        }
+    }
+}
---
+++ SGBL/Controllers/ReporteController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Accesodatos.Context;
+using Accesodatos.Tablas;
+using MediatR;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Aplicación.Logica.Reporte;
+
+namespace SGBL.Controllers
+{
+    public class ReporteController : GeneralController
+    {
+        [HttpPost]
+        public async Task<ActionResult<Unit>>Insertar(Insertar.EjecutaReporte datos)
+        {
+            return await Mediator.Send(datos);
+        }
+    }
+}
