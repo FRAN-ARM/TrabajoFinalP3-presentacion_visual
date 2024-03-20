b00a55a97498e36f9cfa8952a19d08ca7f4a777a
LuisLachapel <luislachapelcentropeguero@gmail.com>
2024-03-19 21:41:41 -0400

    Login implementado

---
+++ SGBL/Controllers/UsuarioController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Accesodatos.Context;
+using Accesodatos.Tablas;
+using MediatR;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Aplicación.Seguridad;
+
+namespace SGBL.Controllers
+{
+    public class UsuarioController : GeneralController
+    {
+        [HttpPost("login")]
+        public async Task<ActionResult<DataUsuario>> Login(Login.EjecutaLogin parametros)
+        {
+            return await Mediator.Send(parametros);
+        }
+    }
+}
