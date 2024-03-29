7922dfa26910b3929a55cedd3b5128539bcdbd4e
E3R3P3 <113480340+E3R3P3@users.noreply.github.com>
2024-03-29 19:25:53 -0400

    PrimerosPaneles

--- obj/Debug/net7.0/TrabajoFinalP3-presentacion_visual.AssemblyInfo.cs
+++ obj/Debug/net7.0/TrabajoFinalP3-presentacion_visual.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrabajoFinalP3-presentacion_visual")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d94c14b1a5a9324b459959530b9306bd4fd80d2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrabajoFinalP3-presentacion_visual")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrabajoFinalP3-presentacion_visual")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
