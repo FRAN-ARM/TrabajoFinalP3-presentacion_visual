e326043cd920bfda9daff0edcd6e0cecc83edeaa
E3R3P3 <113480340+E3R3P3@users.noreply.github.com>
2024-03-31 11:44:04 -0400

    A

--- obj/Debug/net7.0/TrabajoFinalP3-presentacion_visual.AssemblyInfo.cs
+++ obj/Debug/net7.0/TrabajoFinalP3-presentacion_visual.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrabajoFinalP3-presentacion_visual")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21577bcfb6206d54dba51d60b32ce3beb16f9cec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c92269c577c1cd51e06b1121a4ab8d506dafea2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrabajoFinalP3-presentacion_visual")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrabajoFinalP3-presentacion_visual")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
