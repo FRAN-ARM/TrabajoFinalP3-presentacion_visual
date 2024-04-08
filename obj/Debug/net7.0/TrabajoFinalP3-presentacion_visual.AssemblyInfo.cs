613aeb4a423187a692b8ebd04007d0df32abe402
E3R3P3 <113480340+E3R3P3@users.noreply.github.com>
2024-04-07 22:46:13 -0400

    Con Ejemplo de pasar datos entre campos

--- obj/Debug/net7.0/TrabajoFinalP3-presentacion_visual.AssemblyInfo.cs
+++ obj/Debug/net7.0/TrabajoFinalP3-presentacion_visual.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TrabajoFinalP3-presentacion_visual")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b30ca34b49783fd4f2bb81b54e3aa1e6529f8ddd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e50a732ea3e521ddf5ae47cca0b64bb14d987f54")]
 [assembly: System.Reflection.AssemblyProductAttribute("TrabajoFinalP3-presentacion_visual")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TrabajoFinalP3-presentacion_visual")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
