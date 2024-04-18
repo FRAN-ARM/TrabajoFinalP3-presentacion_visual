3b5cc5322cba1c59c8966b58d983d587a31c7418
E3R3P3 <113480340+E3R3P3@users.noreply.github.com>
2024-04-18 15:34:01 -0400

    La de las 3:30

---
+++ Pages/FitroDeTablas.cs
@@ -0,0 +1,6 @@
+﻿public class Filtros
+{
+    public static string Titulo { get; set; }
+    public static string Busqueda { get; set; }
+
+}
---
+++ Pages/Libro.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class LiBro
+{
+    public string id { get; set; }
+    public string Nombre { get; set; }
+    public int Paginas { get; set; }
+    public string Descripcion { get; set; }
+    public int Edicion { get; set; }
+    public DateTime Publicacion { get; set; }
+    public string Autor { get; set; }
+    public string Categoria { get; set; }
+    public string Editorial { get; set; }
+}
