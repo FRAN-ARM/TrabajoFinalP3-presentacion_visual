6b9126ef0b4baa5d60d68403b9f05625dec8256e
E3R3P3 <113480340+E3R3P3@users.noreply.github.com>
2024-04-18 22:52:07 -0400

    El Ultimo

--- Pages/FitroDeTablas.cs
+++ Pages/FitroDeTablas.cs
@@ -3,4 +3,6 @@
     public static string Titulo { get; set; }
     public static string Busqueda { get; set; }
 
+    public static string Visivilidad { get; set; } = "visibility: hidden;";
+
 }
--- Program.cs
+++ Program.cs
@@ -16,6 +16,8 @@ builder.Services.AddScoped<Usuario>();
 builder.Services.AddScoped<Reportes>();
 builder.Services.AddBlazoredSessionStorage();
 
+
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
